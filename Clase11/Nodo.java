977f30214b9906a9d5e43bf7ed51c17852c9df9d
Agus Müller <71037630+El-Tt@users.noreply.github.com>
2026-05-21 14:55:27 -0300

    Add files via upload

---
+++ Clase11/Nodo.java
@@ -0,0 +1,15 @@
+public class Nodo {
+
+    int dato;
+
+    Nodo izquierda;
+    Nodo derecha;
+
+    public Nodo(int dato) {
+
+        this.dato = dato;
+
+        izquierda = null;
+        derecha = null;
+    }
+}
